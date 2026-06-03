9a3166e4db7047dd5c2212a201d9470cff503df9
Maryam <247788554+mariamahmedshalaby1-cmyk@users.noreply.github.com>
2026-06-03 23:22:08 +0300

    pages work on localhost successfully

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
+const path = require('path');
+app.use(express.static(path.join(__dirname)));
 // ─────────────────────────────────────
 // MIDDLEWARE
 // ─────────────────────────────────────
