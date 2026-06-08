d7f30227d7b688ec07e89259d84b9e7b073a7bf6
Razan Ismail <Razan.namila@gmail.com>
2026-06-08 23:20:55 +0300

    fixed the hamburger menu

---
+++ public/js/menu.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const mobileMenu = document.getElementById('mobile-menu');
+  const hamburger = document.getElementById('hamburger');
+  mobileMenu.classList.toggle('open');
+  hamburger.classList.toggle('open');
+}
